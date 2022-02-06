19b2ade98575540cf2b23f4edb09d3ca527910a3
chrisrothwell <33397031+chrisrothwell@users.noreply.github.com>
2022-02-06 15:02:54 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ npm install --silent --no-progress -g npm
 npm install --silent --no-progress -g serverless@2
 serverless info --verbose
 serverless deploy --stage dev --package /$CODEBUILD_SRC_DIR/target/dev --verbose
+aws s3 cp frontend/index.html s3://chrisrothwell.com/mobcp/index.html
+aws s3 cp frontend/index.js s3://chrisrothwell.com/mobcp/index.js

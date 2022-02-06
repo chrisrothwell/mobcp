3e8d4caf37dfa2d005c9e78967d45531c04a4e40
chrisrothwell <33397031+chrisrothwell@users.noreply.github.com>
2022-02-06 13:34:15 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+pwd
+ls
 npm install - g serverless
+pwd
+ls
+sls info --verbose
 serverless info --verbose
 serverless deploy --stage dev --package /$CODEBUILD_SRC_DIR/target/dev -v
